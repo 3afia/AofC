8eda4f78cbf72ebac368067c7fccd56a1a4184e8
Abdellah Lafia <a.3afia@gmail.com>
2022-04-28 23:26:14 +1000

    day 3 part 2 done

--- day03/2.py
+++ day03/2.py
@@ -3,48 +3,39 @@
 ## Adevent of code -- day 03/P02 ----------- By Abdellah Lafia
 ##_____________________________________________________________
 
-#with open('input.txt', 'r') as f:
-#    s = [ x.strip() for x in f.readlines()]
+with open('input.txt', 'r') as f:
+    s = [ x.strip() for x in f.readlines()]
 
-s =[ '00100','11110','10110','10111','10101','01111','00111','11100','10000','11001','00010','01010']
+def molc(a,t,p): # Most or least common
+    o, z = 0, 0 # counters 
+     
+    if len(a) == 2:
+        return 1 if t == 1 else 0 
+    for i in a:
+        if int(i[p]) == 1:
+            o += 1
+        else:
+            z += 1
+    if t == 1:
+        return 1 if o>=z else 0
+    if t == 0:
+        return 0 if o>=z else 1
+           
+def filter_(a, t, p=0):
+    # loop over cols of a 2d array and cmp each element with most or least common    
+    if p == len(a[0]) or len(a) == 1: # exit recursion
+        return a[0]
 
-def molc(): # Most or least common
-    o, z = 0, 0 
-    mc, lc = [], [] 
-      
-    for i in range(len(s[0])):
-        for j in s:
-            if int(j[i]) == 1:
-                o += 1
-            else:
-                z += 1
-        
-        mc.append('1') if o>=z else mc.append('0')
-        lc.append('1') if o<=z else lc.append('0')
-        o,z = 0, 0
-    return mc,lc 
-#________________________^______PART 1_________^__________________________#
-
-def filter_(array1, array2, p=0):
-    if p == len(array2) or len(array1) == 1:
-        return array1[0]
-    new_array = []
- 
-    for i in array1:
-        #check if there is a matching common
-        if len(array2) == 2:
-
-        if i[p] == array2[p]:
-            new_array.append(i)
+    new_a = []
+    c = molc(a,t,p) # most or least common bit
+    for i in a: 
+        if int(i[p]) == c:
+            new_a.append(i)
     p += 1
-    return filter_(new_array, array2, p)
+    return filter_(new_a,t, p)
 
-def lsr(): #Life Support Rating
-    mc, lc = molc()
-    
-    o2 = filter_(s,mc)
-    co2 = filter_(s,lc)
-    
+def lsr():              #Life Support Rating
+    o2 = filter_(s,1)   # 1 for O2, 0 for CO2
+    co2 = filter_(s,0)
     print(int(''.join(o2),2)*int(''.join(co2),2))
 lsr()
-#this is an incomplete solution, will continue tomorrow
